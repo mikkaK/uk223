b339fd6c8bca480924bd887cbe13abdb7d2502ce
Luca <luca.widmer@noseryoung.ch>
2022-02-23 07:44:30 +0100

    small updates

--- src/main/java/com/example/demo/domain/appUser/UserServiceImpl.java
+++ src/main/java/com/example/demo/domain/appUser/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.management.InstanceAlreadyExistsException;
@@ -23,7 +24,8 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     @Autowired
     private final RoleRepository roleRepository;
-
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
     @Override
 //    This method is used for security authentication, use caution when changing this
@@ -53,6 +55,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             throw new InstanceAlreadyExistsException("User already exists");
         }
         else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return userRepository.save(user);
         }
     }
