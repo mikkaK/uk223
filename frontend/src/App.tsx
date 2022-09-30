3b53379219382af3f29128d99a2c1eb9225491a3
Luca <luca.widmer@noseryoung.ch>
2022-09-30 11:40:22 +0200

    base skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>PlaceHolder</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
