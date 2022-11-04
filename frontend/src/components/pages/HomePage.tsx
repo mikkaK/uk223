0acc26dfefa7bb1550cce7686b582a1e2f8cb406
EnderDark1010 <97506394+EnderDark1010@users.noreply.github.com>
2022-11-04 08:20:09 +0100

    Clean homepage

--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -4,20 +4,6 @@ import { TOKEN_LOCAL_STORAGE_KEY } from "../../Contexts/ActiveUserContext";
 import Navbar from "../Atoms/Navbar";
 
 export default function HomePage() {
-  console.log(localStorage.getItem(TOKEN_LOCAL_STORAGE_KEY));
-  axios({
-    method: "GET",
-    url: "http://localhost:8080/user",
-    headers: {
-      Authorization: localStorage.getItem(TOKEN_LOCAL_STORAGE_KEY),
-    },
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
   return (
     <div>
       <Navbar />
