b11a4d7c67189fa4db2e53ef223b2d2968503597
EnderDark1010 <97506394+EnderDark1010@users.noreply.github.com>
2022-11-09 13:58:43 +0100

    pagination

--- frontend/src/components/pages/GroupInspect.tsx
+++ frontend/src/components/pages/GroupInspect.tsx
@@ -7,34 +7,45 @@ import { User } from "../../types/Database/User";
 import { Text } from "../Atoms/Text";
 import { UserDisplay } from "../Molecules/UserDisplay";
 import Navbar from "../Molecules/Navigation/Navbar";
+import { number } from "prop-types";
 
 export function GroupInspect() {
   let { groupId } = useParams();
   const [group, setGroup] = useState<Group>();
   const [users, setUsers] = useState<User[]>();
+  const [page, setPage] = useState<number>(1);
+  const [imagesPerPage, setImagesPerPage] = useState<number>(10);
   let usersElement;
-  useEffect(() => {
-    //Gets the group using the id
-    const getGroup = async function () {
-      api({
-        method: "GET",
-        url: "http://localhost:8080/group/" + groupId,
-      })
-        .then((res) => {
-          setGroup({
-            id: res.data.id,
-            groupName: res.data.groupName,
-            groupMotto: res.data.groupMotto,
-            groupLogo: res.data.groupLogo,
-          });
-          setUsers(res.data.members);
-        })
-        .catch((e) => {
-          console.log(e);
+  const getGroup = async function () {
+    api({
+      method: "GET",
+      url:
+        "http://localhost:8080/group/members" +
+        groupId +
+        "?page=" +
+        (page - 1) +
+        "&size=" +
+        imagesPerPage,
+    })
+      .then((res) => {
+        setGroup({
+          id: res.data.id,
+          groupName: res.data.groupName,
+          groupMotto: res.data.groupMotto,
+          groupLogo: res.data.groupLogo,
         });
-    };
+        setUsers(res.data.members);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  useEffect(() => {
     getGroup();
   }, []);
+  useEffect(() => {
+    getGroup();
+  }, [page, imagesPerPage]);
 
   usersElement = generateUsersElement();
 
@@ -50,11 +61,31 @@ export function GroupInspect() {
             name={group.groupName}
           />
         </div>
+        <input
+          style={pageStyle}
+          type="number"
+          value={imagesPerPage + ""}
+          onChange={(e) => changeNumOfImages(Number.parseInt(e.target.value))}
+        />
+        <div>
+          <button onClick={decrementPage}>&lt;--</button>
+          <input style={pageStyle} type="number" value={page} />
+          <button onClick={incrementPage}>--&gt;</button>
+        </div>
         <div>{usersElement}</div>
       </div>
     );
   } else {
     return <Navbar />;
+  }
+  function decrementPage() {
+    if (page !== 1) {
+      setPage(page - 1);
+    }
+  }
+
+  function incrementPage() {
+    setPage(page + 1);
   }
 
   function generateUsersElement() {
@@ -74,4 +105,11 @@ export function GroupInspect() {
       return <Text text="No user has joined this group yet!" />;
     }
   }
+  function changeNumOfImages(newImageNumber: number) {
+    setImagesPerPage(newImageNumber);
+  }
 }
+
+const pageStyle: React.CSSProperties = {
+  width: "5vw",
+};
