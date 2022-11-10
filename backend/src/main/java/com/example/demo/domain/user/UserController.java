faf605087605da150e8b04654a1d9049010fcdec
EnderDark1010 <97506394+EnderDark1010@users.noreply.github.com>
2022-11-10 13:09:51 +0100

    fix userPermissionEval hasSameId

--- backend/src/main/java/com/example/demo/domain/user/UserController.java
+++ backend/src/main/java/com/example/demo/domain/user/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
     this.logger = logger;
   }
   @PreAuthorize(
-          "hasAuthority('USER_READ') || @userPermissionEvaluator.hasSameId(authentication.principal.user, id)")
+          "hasAuthority('USER_READ') || @userPermissionEvaluator.hasSameId(authentication.principal.user, #id)")
   @GetMapping("/{id}")
   public ResponseEntity<UserDTO> retrieveById(@PathVariable UUID id) {
     logger.trace("fetching user with id: {}", id);
