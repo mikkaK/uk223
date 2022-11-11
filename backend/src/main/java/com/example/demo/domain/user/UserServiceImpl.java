fed867e8c91472026453de06d28c9434ea5c7a46
MikkaK <mikka.kummer@noseryoung.ch>
2022-11-11 15:41:10 +0100

    added new postman collection (fixed version)

--- backend/src/main/java/com/example/demo/domain/user/UserServiceImpl.java
+++ backend/src/main/java/com/example/demo/domain/user/UserServiceImpl.java
@@ -59,12 +59,11 @@ public class UserServiceImpl extends ExtendedServiceImpl<User> implements UserSe
       if(groupId!=null){
         user.setGroup(groupService.findById(groupId));
         logger.info("added user to group");
-        return save(user);
       }else{
         user.setGroup(null);
         logger.info("remove user from group");
-        return save(user);
       }
+      return save(user);
 
     }
     throw new InstanceNotFoundException();
